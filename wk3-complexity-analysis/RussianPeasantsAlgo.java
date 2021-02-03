b4550a65111bc01e6613cca6f71b35da507df84f
Ruth Dooley <ruthdooley@Ruths-MacBook-Pro-2.local>
2021-02-03 20:28:27 +0000

    prac 3 progress

---
+++ wk3-complexity-analysis/RussianPeasantsAlgo.java
@@ -0,0 +1,29 @@
+public class RussianPeasantsAlgo {
+    private RussianPeasantsAlgo() { }
+
+    public static int result(int[] a) {
+        int x = a[0];
+        int y = a[1];
+
+        return x*y;
+    }
+
+
+    /**
+     * Reads in a sequence of integers from a file, specified as a command-line argument;
+     * counts the number of triples sum to exactly zero; prints out the time to perform
+     * the computation.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args)  {
+        In in = new In(args[0]);
+        int[] a = in.readAllInts();
+
+        //Helper file to measure time count function takes to run
+        Stopwatch timer = new Stopwatch();
+        int result = result(a);
+        StdOut.println("elapsed time = " + timer.elapsedTime());
+        StdOut.println(result);
+    }
+}
