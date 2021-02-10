90fed5f718cb46da17a61da03411a77401b57905
RuthDooley <61741718+RuthDooley@users.noreply.github.com>
2021-02-10 12:19:14 +0000

    Update RussianPeasantsAlgo.java
    Update to long int

--- wk3-complexity-analysis/RussianPeasantsAlgo.java
+++ wk3-complexity-analysis/RussianPeasantsAlgo.java
@@ -1,11 +1,11 @@
 public class RussianPeasantsAlgo {
     private RussianPeasantsAlgo() { }
 
-    public static int result(int[] a) {
-        int x = a[0];
-        int y = a[1];
+    public static long result(long[] a) {
+        long x = a[0];
+        long y = a[1];
 
-        int result = 0;
+        long result = 0;
 
         while (y > 0){
             if (y % 2 != 0) {
@@ -19,10 +19,10 @@ public class RussianPeasantsAlgo {
 
     public static void main(String[] args)  {
         In in = new In(args[0]);
-        int[] a = in.readAllInts();
+        long[] a = in.readAllLongs();
 
         long start = System.nanoTime();
-        int result = result(a);
+        long result = result(a);
         long finish = System.nanoTime();
         long timeElapsed = finish - start;
         StdOut.println("elapsed time in nano seconds = " + timeElapsed);
