a36e6d507fef31b2a2d2aa3c4b86d46d32334cc0
RuthDooley <61741718+RuthDooley@users.noreply.github.com>
2021-02-10 13:33:56 +0000

    Update RussianPeasantsAlgo.java
    Second algorithm

--- wk3-complexity-analysis/RussianPeasantsAlgo.java
+++ wk3-complexity-analysis/RussianPeasantsAlgo.java
@@ -16,16 +16,28 @@ public class RussianPeasantsAlgo {
         }
         return result;
     }
+    
+    public static long result1(long[] a) {
+        return a[0] * a[1];
+    }
+
 
     public static void main(String[] args)  {
         In in = new In(args[0]);
         long[] a = in.readAllLongs();
 
-        long start = System.nanoTime();
-        long result = result(a);
-        long finish = System.nanoTime();
-        long timeElapsed = finish - start;
-        StdOut.println("elapsed time in nano seconds = " + timeElapsed);
-        StdOut.println("Result is -> " + result);
+        long start1 = System.nanoTime();
+        long result1 = result(a);
+        long finish1 = System.nanoTime();
+        long timeElapsed1 = finish1 - start1;
+        StdOut.println("Russian Peasant Algo: Elapsed time in nano seconds = " + timeElapsed1);
+        StdOut.println("Result is -> " + result1);
+
+        long start2 = System.nanoTime();
+        long result2 = result1(a);
+        long finish2 = System.nanoTime();
+        long timeElapsed2 = finish2 - start2;
+        StdOut.println("Multiply: Elapsed time in nano seconds = " + timeElapsed2);
+        StdOut.println("Result is -> " + result2);
     }
 }
