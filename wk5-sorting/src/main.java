4846d5efc6ac51cf2cf44002931c875272cc01d0
RuthDooley <61741718+RuthDooley@users.noreply.github.com>
2021-03-14 14:51:19 +0000

    Inputted in sorted array already
    Will update pdf soon

--- wk5-sorting/src/main.java
+++ wk5-sorting/src/main.java
@@ -2,32 +2,36 @@ import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) {
-        int sizeOfArray = 25000;
-        int [] a = RandomNumberGen.randomArray(sizeOfArray);
-        System.out.println("Unsorted Array --> " + Arrays.toString(a));
+        int sizeOfArray = 100;
+        int [] A = RandomNumberGen.randomArray(sizeOfArray);
+        System.out.println("Unsorted Array A--> " + Arrays.toString(A));
 
         long start1 = System.nanoTime();
-        int [] b = SelectionSort.selectionSort(a);
+        int [] a = SelectionSort.selectionSort(A);
         long finish1 = System.nanoTime();
         long timeElapsed1 = finish1 - start1;
         System.out.println("Selection sort elapsed time = " + timeElapsed1);
 
+        int [] B = RandomNumberGen.randomArray(sizeOfArray);
+        System.out.println("Unsorted Array B --> " + Arrays.toString(B));
 
         long start2 = System.nanoTime();
-        int [] c = InsertionSort.insertionSort(a);
+        int [] b = InsertionSort.insertionSort(B);
         long finish2 = System.nanoTime();
         long timeElapsed2 = finish2 - start2;
         System.out.println("Insertion sort elapsed time = " + timeElapsed2);
 
+        int [] C = RandomNumberGen.randomArray(sizeOfArray);
+        System.out.println("Unsorted Array C --> " + Arrays.toString(C));
 
         long start3 = System.nanoTime();
-        int [] d = SillySort.bogoSort(a);
+        int [] c = SillySort.bogoSort(C);
         long finish3 = System.nanoTime();
         long timeElapsed3 = finish3 - start3;
         System.out.println("Bogo sort elapsed time = " + timeElapsed3);
 
-        System.out.println("Result is -> " + Arrays.toString(b));
-        System.out.println("Result is -> " + Arrays.toString(c));
-        System.out.println("Result is -> " + Arrays.toString(d));
+        System.out.println("A Result is -> " + Arrays.toString(a));
+        System.out.println("B Result is -> " + Arrays.toString(b));
+        System.out.println("C Result is -> " + Arrays.toString(c));
     }
 }
